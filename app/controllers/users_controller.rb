5f4f990bfbc9892308a069db818465d45cc86d0c
Genecode <genemoose@mail.ru>
2018-04-24 01:23:54 +0300

    add skloniator helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,14 @@ class UsersController < ApplicationController
     # модели мы просто вызываем метод User.new, который создает модель, не
     # записывая её в базу.
     @users = [
-        User.new(
-            id: 1,
-            name: 'Vadim',
-            username: 'installero',
-            avatar_url: 'https://secure.gravatar.com/avatar/' \
-          '71269686e0f757ddb4f73614f43ae445?s=100'
-        ),
-        User.new(id: 2, name: 'Misha', username: 'aristofun')
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'https://secure.gravatar.com/avatar/' \
+      '71269686e0f757ddb4f73614f43ae445?s=100'
+      ),
+      User.new(id: 2, name: 'Misha', username: 'aristofun')
     ]
   end
 
@@ -26,18 +26,18 @@ class UsersController < ApplicationController
   def show
     # Болванка пользователя
     @user = User.new(
-        name: 'Vadim',
-        username: 'installero',
-        avatar_url: 'https://secure.gravatar.com/avatar/' \
-        '71269686e0f757ddb4f73614f43ae445?s=100'
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/' \
+      '71269686e0f757ddb4f73614f43ae445?s=100'
     )
 
     # Болванка вопросов для пользователя
     @questions = [
-        Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-        Question.new(
-            text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
-        )
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(
+          text: 'В чем смысл жизни?', created_at: Date.parse('27.03.2016')
+      )
     ]
 
     # Болванка для нового вопроса
