a2f3311b8749063463c2c07e5455a6edf574938e
Genecode <genemoose@mail.ru>
2018-04-23 22:41:27 +0300

    less 50

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ActiveRecord::Base
   #Добавляем валидации для домашнего задания
   validates :username, length: { maximum: 40 }
   validates :email, format: { with: /.+@.+\..+/i }
-  validates :username, format: { with: /^([a-z0-9_]+)+/ }
+  validates :username, format: { with: /\A([a-z0-9_]+)+/ }
 
   # Ошибки валидаций можно посмотреть методом errors.
 
