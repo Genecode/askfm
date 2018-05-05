426fc7c44ae3e5262c15be2d6a5f7f9aa3b56a13
Eugene Musinov <genemoose@mail.ru>
2018-05-05 23:20:40 +0300

    create session after create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to root_url, notice: 'Пользователь успешно создан'
+      session[:user_id] = @user.id
+      redirect_to @user, notice: 'Добро пожаловать'
     else
       render 'new'
     end
