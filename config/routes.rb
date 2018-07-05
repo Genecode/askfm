a92166a4d49fe9fa0bce35a4e88f9c3cd0958d8c
Genecode <genecode@yandex.ru>
2018-07-05 17:44:24 +0300

    routes security fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,5 @@ Rails.application.routes.draw do
 
   resources :users
   resources :questions, except: [:show, :new, :index]
-  resources :sessions, only: [:new, :create, :destroy]
-
-  get 'sign_up' => 'users#new'
-  get 'log_out' => 'sessions#destroy'
-  get 'log_in' => 'sessions#new'
-
+  resource :sessions, only: [:new, :create, :destroy]
 end
